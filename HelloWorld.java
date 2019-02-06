f78355cb169103f82d9b8a5e84b33db4cf4130c6
bingxueguo <869169581@qq.com>
2019-02-06 21:25:10 +0800

    master第二次提交HelloWorld.java文件

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
 public class HelloWorld{
 	public static void main(Strin []args){
-		System.out.print("Hello World");
-		System.out.print("If you can do,I can do it so.");
-		System.out.print("迷时师度，悟时自度。");
+		System.out.print("Hello World!");
 	}
 }
