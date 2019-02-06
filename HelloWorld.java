20e3d5973a11849d297940935003e312497ca3db
bingxueguo <869169581@qq.com>
2019-02-06 21:20:31 +0800

    change the HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld{
 	public static void main(Strin []args){
-		System.out.print("Hello World!");
+		System.out.print("Hello World");
+		System.out.print("If you can do,I can do it so.");
+		System.out.print("迷时师度，悟时自度。");
 	}
 }
